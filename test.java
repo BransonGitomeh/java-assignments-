bcc13a18a0e52d61399bf30ebc478d9a2e4d02c1
BransonGitomeh <sirbranson67@gmail.com>
2016-03-07 03:18:21 +0300

    progress on the scanner thing

--- test.java
+++ test.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
+
 class MyClass
 {
     public static void main(String[] args)
      {
-	int x ;
-	x = 400000; 
-       System.out.println("the value of x is " + x);
+	//declare variables that will be used
+	int y;
+	Scanner sc = new Scanner(System.in);
+	//actual code
+	System.out.println("enter the value of y");
+	/**
+	*y will be fed with the result of the object sc, which is an instance of the class Scanner
+	*/
+	y = sc.nextInt();
+	/**
+	*print out the result
+	*/
+	System.out.println("value is " + y);
      }
 }
