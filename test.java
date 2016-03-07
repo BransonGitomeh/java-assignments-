588ee99ac91ab9c9bd84b936e731ae6e538fc9f4
BransonGitomeh <sirbranson67@gmail.com>
2016-03-07 03:53:23 +0300

    completed question 1

--- test.java
+++ test.java
@@ -6,8 +6,25 @@ class MyClass
     public static void main(String[] args)
      {
 	//declare variables that will be used
-	int feet, inches, halfHeight;
+	int feet, inches, halfHeight, choice;
 	Scanner sc = new Scanner(System.in);
+
+	//ask user what he wants the user to enter height as
+	System.out.println("what units do you want to use?");
+	System.out.println("1 - cm");
+	System.out.println("2 - inches");
+	System.out.println("3 - feet");
+
+        choice = sc.nextInt();
+
+	//check the input given
+	if(choice == 1){
+		System.out.println("you seleceted cm");
+	}else if(choice == 2){
+		System.out.println("you selected inches");
+	}else if(choice == 3){
+		System.out.println("you selected feet");
+	}
 	//actual code
 	System.out.println("enter your height in feet");
 	feet = sc.nextInt();
