989668078ba4292fc4a28a23133cbf3dcc1fc766
BransonGitomeh <sirbranson67@gmail.com>
2016-03-07 03:42:28 +0300

    improve taking half inches and height Q1b:answer

--- test.java
+++ test.java
@@ -6,7 +6,7 @@ class MyClass
     public static void main(String[] args)
      {
 	//declare variables that will be used
-	int feet, inches;
+	int feet, inches, halfHeight;
 	Scanner sc = new Scanner(System.in);
 	//actual code
 	System.out.println("enter your height in feet");
@@ -20,5 +20,15 @@ class MyClass
 	*print out the result
 	*/
 	System.out.println("your height in feet is  " + feet + " your height in inches is " + inches);
+     	
+	/*
+	*ask user for half of height in either feet and inches or centimeters 
+	*/
+	System.out.println(" ");
+	System.out.println("enter half your height,in feet and inches");
+        halfHeight = sc.nextInt();
+	
+	System.out.println("half of your height is " + halfHeight);
+	
      }
 }
