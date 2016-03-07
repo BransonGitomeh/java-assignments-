6baeca95c3bbd9e760dbf36ff7e76b19e90fbd7b
BransonGitomeh <sirbranson67@gmail.com>
2016-03-07 03:26:08 +0300

    taking inches and height Q1:answer

--- test.java
+++ test.java
@@ -6,17 +6,19 @@ class MyClass
     public static void main(String[] args)
      {
 	//declare variables that will be used
-	int y;
+	int feet, inches;
 	Scanner sc = new Scanner(System.in);
 	//actual code
-	System.out.println("enter the value of y");
-	/**
-	*y will be fed with the result of the object sc, which is an instance of the class Scanner
-	*/
-	y = sc.nextInt();
+	System.out.println("enter your height in feet");
+	feet = sc.nextInt();
+
+
+	System.out.println("enter your height in inches");
+        inches = sc.nextInt();
+
 	/**
 	*print out the result
 	*/
-	System.out.println("value is " + y);
+	System.out.println("your height in feet is  " + feet + " your height in inches is " + inches);
      }
 }
